b0bec0643ca459a17ba1fad06499832e28a4e60b
subhankarb <subhankarb@INMAC22NXG.local>
2017-08-08 11:58:52 +0530

    change graphlab create version

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ pip install "ipython[notebook]"
 pip install -U ipyparallel
 pip install -U scikit-learn
 pip install -U pandas
-pip install --upgrade --no-cache-dir https://get.graphlab.com/GraphLab-Create/1.10.1/neo20iitkgp@gmail.com/FF43-95E1-27DC-3E90-1E67-D12A-A6BD-9A2D/GraphLab-Create-License.tar.gz
+pip install --upgrade --no-cache-dir https://get.graphlab.com/GraphLab-Create/2.1/neo20iitkgp@gmail.com/FF43-95E1-27DC-3E90-1E67-D12A-A6BD-9A2D/GraphLab-Create-License.tar.gz
 pip install -U matplotlib
